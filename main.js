0e162859df36d0d4310acd67372eb40b5ca2e985
Anajuju2011 <118820733+Anajuju2011@users.noreply.github.com>
2023-08-31 09:24:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
 var SpeechRecognition = window.webkitSpeechRecognition; /* Explicação: a variável SpeechRecognition contém a API webkitSpeechRecognition (API Web Speech) que é usado para reconhecer o que estamos falando e converter o que foi dito em texto. */
 
-/* Da mesma forma que criamos um humano, crie um novo API */
+var recognition = new SpeechRecognition();
 
 
 var Textbox = document.getElementById("textbox");
 
-function () /* Chamar a função start */
+function start()
 {
     Textbox.innerHTML = ""; /* Explicação: sempre que o botão iniciar é pressionado, queremos que a área de texto fique vazia. */
-    /* Chamar abaixo a função start() para API Web Speech que criamos */
+   recognition.start();
 
 }  
 
-recognition. = function(event) /* Chamar a função onresult. A função onresult contém todos os valores da fala convertidos em texto. */
+recognition.onresult = function(event) 
 {
 
     console.log(event); 
    
-   var Content = event.results[0][0].transcript; /* Explicação: event.results[0][0].transcript é o local no console que tem nossa saída de voz para
-   texto, portanto, vamos armazená-la dentro de uma variável e daremos o nome de Content */
+   var Content = event.results[0][0].transcript; 
    
-       Textbox.innerHTML = ; /* Chamar a variável Content */
-       console.log(); /* Chamar a variável Content */
+       Textbox.innerHTML = Content ; 
+       console.log(Content);
 
-        
+       speak();  
 }  /* Fim da aula 98 */ 
+
+function speak()
+{
+var synth = window.speechSynthesis;
+speakData = "Tirando sua selfie em 2 segundos";
+var utterThis =  new SpeechSynthesisUtterance(speakData);
+synth.speak(utterThis);
+Webcam.attach(camera);
+}
+
+camera = document.getElementById("camera");
+ Webcam.set
+(
+{
+width:360,
+height:250,
+image_format:'jpeg',
+jpeg_quality:90
+}
+);
