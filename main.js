cb3ea33dcbb94d8812d8cd820ae13c1a47436575
Anajuju2011 <118820733+Anajuju2011@users.noreply.github.com>
2023-09-08 10:25:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -21,10 +21,12 @@ recognition.onresult = function(event)
    
        Textbox.innerHTML = Content ; 
        console.log(Content);
-
-       speak();  
-}  /* Fim da aula 98 */ 
-
+       if (Content =="tire minha selfie")
+        {
+        console.log("tirando sua selfie...");
+        speak();  
+        } 
+}
 function speak()
 {
 var synth = window.speechSynthesis;
@@ -32,6 +34,12 @@ speakData = "Tirando sua selfie em 2 segundos";
 var utterThis =  new SpeechSynthesisUtterance(speakData);
 synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout(function()
+{
+ takeSelfie();
+ save();
+}, 5000);
+
 }
 
 camera = document.getElementById("camera");
@@ -44,3 +52,21 @@ image_format:'jpeg',
 jpeg_quality:90
 }
 );
+
+function takeSelfie()
+{
+Webcam.snap(function(data_uri)
+{
+    document.getElementById("result").innerHTML= '<img id="selfieImage" src="'+data_uri+'"/>';
+});
+
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfieImage").src;
+    link.href = image;
+    link.click();
+
+}
